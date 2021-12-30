65674770b7ea74d419de646555390e18c9decb90
lg <ligang@cdhwkj.cn>
2021-12-30 10:27:17 +0800

    fix:弱网情况下导致的请求响应顺序问题

--- lib/src/signal_interceptor.dart
+++ lib/src/signal_interceptor.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:hwkj_api_core/hwkj_api_core.dart';
-import 'package:uuid/uuid.dart';
 
 import 'constants.dart';
 import 'signal_channel.dart';
@@ -15,21 +14,26 @@ class SignalInterceptor extends Interceptor {
   Duration timeout;
   @override
   Future onRequest(RequestOptions options) async {
+    final requestId = DateTime.now().millisecondsSinceEpoch.toString();
     options.headers.addAll({
       HeaderSignalRCallbackKey: kRequestResponseChannelKey,
-      HeaderSignalRTypeKey: Uuid().v4(),
+      // HeaderSignalRTypeKey: Uuid().v4(),
+      HeaderSignalRTypeKey: requestId,
     });
+    options.extra[requestId] = _add202Handler(requestId);
 
     return options;
   }
 
   @override
   Future onResponse(Response response) async {
+    final requestId = response.request.headers[HeaderSignalRTypeKey];
     if (response.statusCode == 200) {
       final r = ApiClient.globalParseResponseData(response);
       if (r.type == 202) {
-        final r1 =
-            await _handle202(response.request.headers[HeaderSignalRTypeKey]);
+        // final r1 =
+        //     await _handle202(response.request.headers[HeaderSignalRTypeKey]);
+        final r1 = await response.request.extra[requestId];
 
         if (r1.success) {
           // 成功后，r1.data 中包含 Code,Content,Data字段
@@ -41,13 +45,15 @@ class SignalInterceptor extends Interceptor {
             ..data = r1.data;
           response.data = data.toJson();
         }
+      } else {
+        _remove202Handler(requestId);
       }
     }
     return response;
   }
 
   /// 处理202响应
-  Future<APIResult> _handle202(String requestId) async {
+  Future<APIResult> _add202Handler(String requestId) async {
     final resultCompleter = Completer<APIResult>();
 
     if (SignalChannel.globalSignalChannel == null)
@@ -57,5 +63,9 @@ class SignalInterceptor extends Interceptor {
 
     return resultCompleter.future.timeout(timeout ?? _defaultTimeout,
         onTimeout: () => APIResult.failure('请求超时', null));
+  }
+
+  void _remove202Handler(String requestId) {
+    SignalChannel.globalSignalChannel?.removeRequest(requestId);
   }
 }
