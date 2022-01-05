7e755519753f737c871508e167fbb1dd28df4497
lg <ligang@cdhwkj.cn>
2022-01-05 13:52:48 +0800

    feat:add hooks

--- lib/src/signal_channel.dart
+++ lib/src/signal_channel.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:hwkj_api_core/hwkj_api_core.dart';
 import 'package:lg_signalr_client/lg_signalr_client.dart';
 import 'package:logger/logger.dart';
@@ -7,6 +8,23 @@ import 'package:logging/logging.dart' as log;
 
 import 'constants.dart';
 import 'error.dart';
+
+typedef SignalInterceptorHook = void Function(String requestId);
+
+class SignalChannelHooks {
+  SignalInterceptorHook didAddRequest;
+  SignalInterceptorHook didReceiveResponse;
+  SignalInterceptorHook didRemoveRequest;
+  SignalInterceptorHook didReceiveResponseWithoutRequest;
+}
+
+enum SignalChannelState {
+  Disconnected,
+
+  Connected,
+
+  Reconnecting,
+}
 
 class SignalChannel {
   static SignalChannel globalSignalChannel;
@@ -22,9 +40,19 @@ class SignalChannel {
   /// 待处理请求集合
   Map<String, Completer<APIResult>> _requestMap = {};
 
+  ValueNotifier<SignalChannelState> stateNotifier;
+
+  SignalChannelHooks hooks;
+
   SignalChannel(String url, AccessTokenFactory accessTokenFactory)
       : this._url = url,
-        this._accessTokenFactory = accessTokenFactory;
+        this._accessTokenFactory = accessTokenFactory {
+    stateNotifier = ValueNotifier(SignalChannelState.Disconnected);
+  }
+
+  void dispose() {
+    stateNotifier.dispose();
+  }
 
   /// 设置打印日志
   SignalChannel configWithLogger(Logger logger) {
@@ -33,23 +61,30 @@ class SignalChannel {
   }
 
   Future start() async {
-    if (_hubConnection?.state == HubConnectionState.Connected) return;
+    try {
+      if (_hubConnection?.state == HubConnectionState.Connected) return;
 
-    _hubConnection = HubConnectionBuilder()
-        .withUrl(_url,
-            options: HttpConnectionOptions(
-              transport: HttpTransportType.WebSockets,
-              accessTokenFactory: _accessTokenFactory,
-              skipNegotiation: true,
-            ))
-        .configureLogging(log.Logger('$this'))
-        .build();
+      _hubConnection = HubConnectionBuilder()
+          .withUrl(_url,
+              options: HttpConnectionOptions(
+                transport: HttpTransportType.WebSockets,
+                accessTokenFactory: _accessTokenFactory,
+                skipNegotiation: true,
+              ))
+          .configureLogging(log.Logger('$this'))
+          .build();
 
-    _hubConnection.on(kRequestResponseChannelKey, _handleRequestResponse);
-    _hubConnection.onClose(_connectionClose);
+      _hubConnection.on(kRequestResponseChannelKey, _handleRequestResponse);
+      _hubConnection.onClose(_connectionClose);
 
-    _needReconnect = true;
-    return _hubConnection.start();
+      _needReconnect = true;
+      await _hubConnection.start();
+      stateNotifier.value = SignalChannelState.Connected;
+    } catch (e, trace) {
+      _logger.e('signal_channel start', e, trace);
+      stateNotifier.value = SignalChannelState.Disconnected;
+      throw e;
+    }
   }
 
   Future stop({bool autoReconnect = false}) async {
@@ -66,16 +101,21 @@ class SignalChannel {
     _requestMap.clear();
 
     if (preHub?.state == HubConnectionState.Connected) await preHub?.stop();
+    stateNotifier.value = SignalChannelState.Disconnected;
   }
 
   Future reconnect() async {
     if (_hubConnection?.state == HubConnectionState.Connected) return;
+    stateNotifier.value = SignalChannelState.Reconnecting;
     return start();
   }
 
   void _connectionClose(Exception error) {
     _logger?.e('', error);
-    if (_needReconnect) reconnect();
+    if (_needReconnect)
+      reconnect();
+    else
+      stateNotifier.value = SignalChannelState.Disconnected;
   }
 
   void on(String methodName, MethodInvocationFunc newMethod) {
@@ -92,12 +132,14 @@ class SignalChannel {
   void addRequest(String requestId, Completer<APIResult> completer) {
     _logger?.v('添加待处理请求 $requestId');
     _requestMap[requestId] = completer;
+    hooks?.didAddRequest?.call(requestId);
   }
 
   /// 移除请求
   void removeRequest(String requestId) {
     _logger?.v('移除待处理请求 $requestId');
     _requestMap.remove(requestId);
+    hooks?.didRemoveRequest?.call(requestId);
   }
 
   /// 处理请求的响应结果
@@ -106,8 +148,11 @@ class SignalChannel {
     Map data = arguments.first;
     final String requestId = data['SignalrType'];
     if (_requestMap.containsKey(requestId)) {
+      hooks?.didReceiveResponse?.call(requestId);
       final complete = _requestMap.remove(requestId);
       if (!complete.isCompleted) complete.complete(APIResult.success(data));
+    } else {
+      hooks?.didReceiveResponseWithoutRequest?.call(requestId);
     }
   }
 
